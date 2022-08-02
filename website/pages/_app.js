dd5b573b78b4abf311282a2068cb798e5787e2b3
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-02 18:48:28 +0430

    Website: snackbar.scss imported

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/index.scss'
 import '../styles/main.scss'
+import 'awesome-snackbar/src/snackbar.scss'
 
 export default function MyApp({ Component, pageProps }){
     return <Component {...pageProps} />
