6309314ecfbdedc949ace5d5811b93d11d12eb23
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-03 09:14:11 +0430

    Package: sass import removed

--- package/index.js
+++ package/index.js
@@ -1,6 +1,2 @@
-//.scss
-// import './src/snackbar.scss'
-
-//js:
 const SnackbarClass = require('./src/snackbar');
 module.exports = (massage, parameters) => {return new SnackbarClass({massage, ...parameters});}
