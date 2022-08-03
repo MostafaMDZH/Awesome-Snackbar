162fdf96919110fd6d38335a9c1bb9982a718a2d
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-03 09:16:29 +0430

    Website: copy function created

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,4 +1,40 @@
+import Snackbar from 'awesome-snackbar';
+
 export default function Main(){
+
+    //copyTextToClipboard:
+	const copyTextToClipboard = (text) => {
+		if(!navigator.clipboard){
+			fallbackCopyTextToClipboard(text);
+		}else{
+			navigator.clipboard.writeText(text).then(function(){
+                Snackbar('Copied to clipboard');
+			},function(err){
+                Snackbar('cannot copy');
+			});
+		}
+	}
+
+	//fallbackCopyTextToClipboard:
+	const fallbackCopyTextToClipboard = (text) => {
+		let textArea = document.createElement("textarea");
+		textArea.value			= text;
+		textArea.style.top		= "0";//avoid scrolling to bottom:
+		textArea.style.left		= "0";
+		textArea.style.position	= "fixed";
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+		try{
+			document.execCommand('copy');
+            Snackbar('Copied to clipboard');
+		}catch(err){
+            Snackbar('cannot copy');
+		}
+		document.body.removeChild(textArea);
+	}
+
+    //render:
     return (
         <div id='window'>
             <div id='container'>
@@ -23,6 +59,7 @@ export default function Main(){
                         <h3 id='awesome'><a href='/'>Awesome</a></h3>
                         <h1 id='snackbar'><a href='/'>Snackbar</a></h1>
                         <a id='version'>V1.0.0</a>
+
                     </div>
 
                 </div>
