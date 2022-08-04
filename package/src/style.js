9f06d1158542ee153303e53f99459b5b7cf82e71
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-04 12:30:42 +0430

    Package.style: icon added

--- package/src/style.js
+++ package/src/style.js
@@ -1,3 +1,4 @@
+export default `
 .snackbar {
   position: fixed;
   transition: top 400ms ease 0s, bottom 400ms ease 0s, margin-top 300ms ease 0s, margin-bottom 300ms ease 0s, opacity 150ms ease 150ms;
@@ -9,23 +10,31 @@
   padding: 10px 20px;
   border-radius: 3px;
   background-color: rgb(58, 58, 58);
-  font-size: 0.87rem;
   box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1), 0 3px 3px rgba(0, 0, 0, 0.05);
   display: flex;
   color: rgb(250, 250, 250);
   align-items: center;
-  gap: 15px;
+  gap: 10px;
 }
 .snackbar > .container * {
   box-sizing: border-box;
 }
-.snackbar > .container > #massage {
-  line-height: 22px;
+.snackbar > .container > .icon {
+  width: 20px;
+  height: 20px;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center center;
+  display: none;
+}
+.snackbar > .container > .massage {
+  font-size: 0.87rem;
 }
 .snackbar > .container > .actionButton {
   height: 100%;
-  background-color: transparent;
   padding: 5px 3px;
+  background-color: transparent;
+  font-size: 0.87rem;
   color: #F7FF00;
   border: none;
   outline: none;
@@ -78,3 +87,4 @@
     max-width: calc(90% - 12px);
   }
 }/*# sourceMappingURL=snackbar.css.map */
+`;
