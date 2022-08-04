30c19031e7f2647c13ca839a9c3ba5fe61ba9586
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-04 12:40:21 +0430

    Website: snackbar.scss removed

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/index.scss'
 import '../styles/main.scss'
-import 'awesome-snackbar/src/snackbar.css'
 
 export default function MyApp({ Component, pageProps }){
     return <Component {...pageProps} />
