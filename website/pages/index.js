add7b02dfa9bdd205c687d2d7081d0b4a9fc0c2e
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-08-13 13:30:03 +0430

    Website: duplicated id changed

--- website/pages/index.js
+++ website/pages/index.js
@@ -461,7 +461,7 @@ export default function Main(){
             <footer id='footer'>
                 <p>Made by <a href='https://mostafa-mdzh.ir'>Mostafa Mohammadzadeh</a></p>
                 <p id='dash'>-</p>
-                <p id='github'>Source on <a href='https://github.com/MostafaMDZH/Awesome-Snackbar'>Github</a></p>
+                <p id='githubLink'>Source on <a href='https://github.com/MostafaMDZH/Awesome-Snackbar'>Github</a></p>
                 <p id='awesomeComponents'>From <Link href='/'><a>Awesome Components</a></Link></p>
             </footer>
 
